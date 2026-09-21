53ab1392444ffe54498e120b309500f46e0043c1
Mohammad-AboZamel <moabo9939@usn.no>
2026-09-21 14:50:41 +0200

    Update text string in eksempel-5.php

--- tema03/eksempler/eksempel-5.php
+++ tema03/eksempler/eksempel-5.php
@@ -2,7 +2,7 @@
 /*
 /*    Programmet deler opp en tekststreng ved bruk av oppdelingsfunksjonen explode
 */
-  $tekst="Geir Bjarvin Universitetslektor USN";
+  $tekst="Mohammad er Student ved USN";
   
   $del=explode (" " , $tekst);     
   $fornavn=$del[0];     
